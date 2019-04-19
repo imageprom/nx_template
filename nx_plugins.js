23bce2e35403765a5b084e97482e7f4e14b42fa7
imageprom <necris@imageprom.com>
2019-04-19 15:56:33 +0300

    Fixed scrollbar.width()

--- nx_plugins.js
+++ nx_plugins.js
@@ -4,7 +4,7 @@ $(function(){
 var s_width = 1025;
 function AddNXAdaptiveNavication() {
     var nxNavlen = $('#NXadaptiveNavigation').length, 
-        nxScreen = $(window).width(); 
+        nxScreen = nxScreen = window.innerWidth;
 
     if(nxNavlen == 0 && nxScreen <= s_width) {
         var navTitle = $('.logo').text(),
